12cc967fbf71a1a7ff51db64a2f001de236b5bf0
Glitch (slender-soapy-amusement) <none>
2020-10-16 22:38:21 +0000

    🙋🕍 Checkpoint ./komutlar/couple.js:23903385/1436 ./komutlar/alone.js:23903385/1407 ./komutlar/sorumluekip.js:23903385/1419 ./ayarlar.json:23903385/126

---
+++ komutlar/couple.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const db = require('quick.db');
+exports.run = (client, message, args) => {
+  if(!message.member.roles.get("YÖNETİM ROL ID") && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.sendEmbed(new Discord.RichEmbed().addField(`Yetersiz Yetki` , `Yeterli Yetkiniz Olmadığı için Bu Komutu Kullanamazsınız`).setColor("RANDOM").setTimestamp()).then(m => m.delete(5000));
+  else {
+    let member = message.mentions.users.first() || client.users.get(args.join(' '))
+      if(!member) return message.channel.send(new Discord.RichEmbed().setColor('RANDOM').addField("Hatalı Kullanım",`Lütfen Bir Kullanıcı Etiketleyiniz`).setFooter(message.author.username, message.author.avatarURL)).then(m => m.delete(10000));
+    const kullanıcı = message.guild.member(member)
+    kullanıcı.addRole("756251694169456640")//Change
+    const embed = new Discord.RichEmbed()
+    .setTitle("Volantis yönetim | ROL VERME")
+    .setColor("RANDOM")
+    .addField("Yetki Verildi", `<@!${kullanıcı.user.id}> Adlı Kullanıcıya Yetki Verildi`)
+    .setFooter('Volantis / Rol Verme Botu')
+    message.channel.send(embed)
+  }
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["couple"],
+  permLevel: 0
+};
+exports.help = {
+  name: "couple",
+  description: "Kullanıcıyı Yetki Verir",
+  usage: "couple"
+};
